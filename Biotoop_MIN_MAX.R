d8fd334fb5c00a4d24c93283bb4debe5c5e9e2db
WouterVanReeth <wouter.vanreeth@inbo.be>
2026-08-13 10:15:30 +0200

    aanpassing gemini-figuur aan ecosysteemdata (definities, kleuren en data analyse C.Wils)

--- Biotoop_MIN_MAX.R
+++ Biotoop_MIN_MAX.R
@@ -6,29 +6,36 @@ library(ggplot2)
 
 # 1. Voorbeelddata
 df <- data.frame(
-  biotoop = c("Bos", "Heide", "Moeras", "Grasland"),
-  OPP     = c(150, 80, 45, 120),
-  MINOPP  = c(120, 65, 30, 100),
-  MAXOPP  = c(170, 95, 60, 140)
+  ecosysteem = c("3.1 Modified grassland", "3.2 Semi-natural grassland", "4.1 Broadleaved deciduous forests", "4.2 Coniferous forests", "4.3 Forest shrub","5.1 Temperate shrub heathland", "6.2 Inland sand dunes", "7.1 Inland wetlands on mineral soil", "7.2 Mires, bogs and fens", "10.2 Intertidal flats", "11.2 Coastal dunes and beaches"),
+  OPP     = c(187853, 79634, 87777, 54924, 10221, 10580, 133, 3333, 301, 1301, 4520),
+  MINOPP  = c(165588, 77944, 83227, 47188, 9213, 9968, 117, 2570, 266, 1299, 4376),
+  MAXOPP  = c(195786, 98443, 97736, 56294, 14555, 12016, 314, 4713, 570, 1344, 4571)
 )
 
 # 2. Definieer je eigen hex-kleurcodes per biotoop
-kleuren_biotoop <- c(
-  "Bos"      = "#2e6f40", # Donkergroen
-  "Heide"    = "#8e44ad", # Paars
-  "Moeras"   = "#3498db", # Blauw
-  "Grasland" = "#2ecc71"  # Lichtgroen
+kleuren_ecosysteem <- c(
+  "3.1 Modified grassland"      = "#CCFFCC", # Bleekgroen
+  "3.2 Semi-natural grassland"    = "#AAFF00", # Felgroen
+  "4.1 Broadleaved deciduous forests"   = "#00B050", # Donkergroen
+  "4.2 Coniferous forests" = "#00CC66",  # Diepgroen
+  "4.3 Forest shrub" = "#33CC33",  # Groen
+  "5.1 Temperate shrub heathland" = "#E49EDD",  # Lichtpaars
+  "6.2 Inland sand dunes" = "#FFD37F",  # Geelbruin
+  "7.1 Inland wetlands on mineral soil" = "#CDAA66",  # Lichtbruin
+  "7.2 Mires, bogs and fens" = "#996633",  # Bruin
+  "10.2 Intertidal flats" = "#785829",  # Donkerbruin
+  "11.2 Coastal dunes and beaches" = "#FF73DF"  # Roos
 )
 
 # 3. Figuur maken
-ggplot(df, aes(y = reorder(biotoop, OPP), x = OPP, fill = biotoop)) +
+ggplot(df, aes(y = reorder(ecosysteem, OPP), x = OPP, fill = ecosysteem)) +
   geom_col(width = 0.6, alpha = 0.85) +
   geom_errorbar(aes(xmin = MINOPP, xmax = MAXOPP), width = 0.2, color = "black") +
-  scale_fill_manual(values = kleuren_biotoop) +
+  scale_fill_manual(values = kleuren_ecosysteem) +
   labs(
-    title = "Oppervlakteschatting per biotoop",
+    title = "Oppervlaktevergelijking raster- en vectordata",
     x = "Oppervlakte",
-    y = "Biotoop"
+    y = "Ecosysteem"
   ) +
   theme_minimal() +
   theme(legend.position = "none") # Verbergt de legende, want de Y-as toont de namen al
