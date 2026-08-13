6616ccb2da60b360494e2da65278127cba77563b
WouterVanReeth <wouter.vanreeth@inbo.be>
2026-08-13 16:25:26 +0200

    afwerking figuren (lay-out en leesbaarheid)

--- Biotoop_MIN_MAX.R
+++ Biotoop_MIN_MAX.R
@@ -4,20 +4,30 @@
 
 library(ggplot2)
 
-# 1. Voorbeelddata
-df <- data.frame(
-  ecosysteem = c("3.1 Modified grassland", "3.2 Semi-natural grassland", "4.1 Broadleaved deciduous forests", "4.2 Coniferous forests", "4.3 Forest shrub","5.1 Temperate shrub heathland", "6.2 Inland sand dunes", "7.1 Inland wetlands on mineral soil", "7.2 Mires, bogs and fens", "10.2 Intertidal flats", "11.2 Coastal dunes and beaches"),
-  OPP     = c(187853, 79634, 87777, 54924, 10221, 10580, 133, 3333, 301, 1301, 4520),
-  MINOPP  = c(165588, 77944, 83227, 47188, 9213, 9968, 117, 2570, 266, 1299, 4376),
-  MAXOPP  = c(195786, 98443, 97736, 56294, 14555, 12016, 314, 4713, 570, 1344, 4571)
+# 1. data, opgesplitst voor grote en kleine ecosystemen
+df_gr <- data.frame(
+  ecosysteem_gr = c("3.1 Modified grassland", "3.2 Semi-natural grassland", "4.1 Broadleaved deciduous forests", "4.2 Coniferous forests"),
+  OPP_gr     = c(187853, 79634, 87777, 54924),
+  MINOPP_gr  = c(165588, 77944, 83227, 47188),
+  MAXOPP_gr  = c(195786, 98443, 97736, 56294)
+)
+
+df_kl <- data.frame(
+  ecosysteem_kl = c("4.3 Forest shrub","5.1 Temperate shrub heathland", "6.2 Inland sand dunes", "7.1 Inland wetlands on mineral soil", "7.2 Mires, bogs and fens", "10.2 Intertidal flats", "11.2 Coastal dunes and beaches"),
+  OPP_kl     = c(10221, 10580, 133, 3333, 301, 1301, 4520),
+  MINOPP_kl  = c(9213, 9968, 117, 2570, 266, 1299, 4376),
+  MAXOPP_kl  = c(14555, 12016, 314, 4713, 570, 1344, 4571)
 )
 
 # 2. Definieer je eigen hex-kleurcodes per biotoop
-kleuren_ecosysteem <- c(
+kleuren_ecosysteem_gr <- c(
   "3.1 Modified grassland"      = "#CCFFCC", # Bleekgroen
   "3.2 Semi-natural grassland"    = "#AAFF00", # Felgroen
   "4.1 Broadleaved deciduous forests"   = "#00B050", # Donkergroen
-  "4.2 Coniferous forests" = "#00CC66",  # Diepgroen
+  "4.2 Coniferous forests" = "#00CC66"  # Diepgroen
+  )
+
+kleuren_ecosysteem_kl <- c(
   "4.3 Forest shrub" = "#33CC33",  # Groen
   "5.1 Temperate shrub heathland" = "#E49EDD",  # Lichtpaars
   "6.2 Inland sand dunes" = "#FFD37F",  # Geelbruin
@@ -28,17 +38,46 @@ kleuren_ecosysteem <- c(
 )
 
 # 3. Figuur maken
-ggplot(df, aes(y = reorder(ecosysteem, OPP), x = OPP, fill = ecosysteem)) +
+ggplot(df_gr, aes(y = reorder(ecosysteem_gr, OPP_gr), x = OPP_gr, fill = ecosysteem_gr)) +
   geom_col(width = 0.6, alpha = 0.85) +
-  geom_errorbar(aes(xmin = MINOPP, xmax = MAXOPP), width = 0.2, color = "black") +
-  scale_fill_manual(values = kleuren_ecosysteem) +
+  geom_errorbar(aes(xmin = MINOPP_gr, xmax = MAXOPP_gr), width = 0.2, color = "black") +
+  scale_fill_manual(values = kleuren_ecosysteem_gr) +
+  scale_x_continuous(
+    labels = scales::label_number(big.mark = ".", decimal.mark = ","),
+    expand = expansion(mult = c(0, 0.15))
+    ) +
+  coord_cartesian(clip = "off") +
   labs(
-    title = "Oppervlaktevergelijking raster- en vectordata",
-    x = "Oppervlakte",
-    y = "Ecosysteem"
+    title = "Comparison extent raster and vector data (a)",
+    x = "Extent (hectare)",
+    y = "Ecosystem"
   ) +
   theme_minimal() +
-  theme(legend.position = "none") # Verbergt de legende, want de Y-as toont de namen al
+  theme(
+    legend.position = "none", # Verbergt de legende, want de Y-as toont de namen al
+    plot.margin = margin(t = 10, r = 25, b = 10, l = 10)
+    )
+
+ggplot(df_kl, aes(y = reorder(ecosysteem_kl, OPP_kl), x = OPP_kl, fill = ecosysteem_kl)) +
+  geom_col(width = 0.6, alpha = 0.85) +
+  geom_errorbar(aes(xmin = MINOPP_kl, xmax = MAXOPP_kl), width = 0.2, color = "black") +
+  scale_fill_manual(values = kleuren_ecosysteem_kl) +
+  scale_x_continuous(
+    labels = scales::label_number(big.mark = ".", decimal.mark = ","),
+    expand = expansion(mult = c(0, 0.15))
+    ) +
+  coord_cartesian(clip = "off") +
+  labs(
+    title = "Comparison extent raster andn vector data (b)",
+    x = "Extent (hectare)",
+    y = "Ecosystem"
+  ) +
+  theme_minimal() +
+  theme(
+    legend.position = "none", # Verbergt de legende, want de Y-as toont de namen al
+    plot.margin = margin(t = 10, r = 25, b = 10, l = 10)
+    )
+
 
 # probeersel 1 gemini
 
